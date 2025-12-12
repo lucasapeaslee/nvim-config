17a0f796ebbe8afafd35b1e86830904e16aea821
Lucas Peaslee <lucasapeaslee@gmail.com>
2025-12-11 21:08:21 -0600

    Add nvim-tree

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+
 require("config.lazy")
 
 vim.opt.number = true
@@ -5,3 +10,5 @@ vim.opt.number = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
+
+vim.keymap.set("n","n",":NvimTreeToggle<CR>")
